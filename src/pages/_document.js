e8318a9ea4c1f31b3df43ae15d8d9b05a1022635
shobhitsarin <shobhit.sarin@sapient.com>
2020-04-13 00:35:23 +0530

    Change in package.json and few minor name changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export const htmlTemplate = ( appComponent,initalProps) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta name="Description" content="This is HN_REACT_SSR_CLONE">
             <meta charset="utf-8">
-            <title>Think App</title>
+            <title>HN_REACT_SSR_CLONE App</title>
             <link rel="stylesheet" type="text/css" href="./styles.css" />
         </head>
         <body>
